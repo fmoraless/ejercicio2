27be5b16e0c620a4403d802a8631c28bea3755f4
Francisco Morales Sánchez <franticzero@gmail.com>
2017-04-27 17:24:00 -0300

    Accion sumar elementos del array

--- src/ejercicio_2/Ejercicio_2.java
+++ src/ejercicio_2/Ejercicio_2.java
@@ -10,6 +10,7 @@ public class Ejercicio_2 {
      */
     public static void main(String[] args) {
         int arr[], tam;
+		
  
         tam = Integer.parseInt(JOptionPane.showInputDialog(null, "De que tamaño desea el arreglo: "));
         arr = new int[tam];
@@ -18,6 +19,16 @@ public class Ejercicio_2 {
             arr[i]=(int)(Math.random()*9+0);
             System.out.println("Posición "+i+"--> "+arr[i]);
         }
+		
+		summarArreglo(arr);
     }
+	
+	public static void summarArreglo(int arr1[]){
+		int total = 0;
+		for(int i=0; i<arr1.length; i++){
+            total+=arr1[i];
+        }
+		System.out.println("La suma de los elementos del arreglo es: "+total);
+	}
     
 }
